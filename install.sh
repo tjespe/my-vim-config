89a96bdfd36118b09e3c3705affbb2813742750e
tjespe <tordjohanespe@gmail.com>
2018-01-26 19:06:52 +0100

    Tried to remove unnecessary text printed to vim buffer on updating

--- install.sh
+++ install.sh
@@ -2,9 +2,15 @@
 cd "${0%/*}"
 if [ ! -f bundle/last-update ] || (($(date +%s) > $(cat bundle/last-update 2> /dev/null) + 10))
 then
-  git pull
-  git submodule init
-  git submodule update
+  if [ -f bundle/last-update ]; then
+    git pull > /dev/null
+    git submodule init > /dev/null
+    git submodule update > /dev/null
+  else
+    git pull
+    git submodule init
+    git submodule update
+  fi
   date +%s > bundle/last-update
   vim -E -c PluginInstall -c q -c q -c q
   echo "Installation complete!"
