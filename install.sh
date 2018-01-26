892162ead14e475479c7f32d62a0c74db5b41dbc
tjespe <tordjohanespe@gmail.com>
2018-01-26 19:15:16 +0100

    Revert probably unnecessary commit: "Tried to remove unnecessary text printed to vim buffer on updating"
    This reverts commit d32e8baa15ef2eb64cf7eab41b80e87aebef1f3e.

--- install.sh
+++ install.sh
@@ -2,15 +2,9 @@
 cd "${0%/*}"
 if [ ! -f bundle/last-update ] || (($(date +%s) > $(cat bundle/last-update 2> /dev/null) + 10))
 then
-  if [ -f bundle/last-update ]; then
-    git pull > /dev/null
-    git submodule init > /dev/null
-    git submodule update > /dev/null
-  else
-    git pull
-    git submodule init
-    git submodule update
-  fi
+  git pull
+  git submodule init
+  git submodule update
   date +%s > bundle/last-update
   vim -E -c PluginInstall -c q -c q -c q
   echo "Installation complete!"
